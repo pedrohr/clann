99866cfea6efaa67b38cc87066656c16fdf45a00
Pedro Assis <pedroh2306@gmail.com>
2013-04-09 16:16:15 -0300

    Clustering triples and memory optimization

--- spec/clann_spec.rb
+++ spec/clann_spec.rb
@@ -1,17 +1,65 @@
 require 'test/unit'
 require './src/clann.rb'
 
+# Mocks
+class Clann
+  def free_the_fish
+    return true
+  end
+end
+
 class ClannTest < Test::Unit::TestCase
   def setup
-    @mapping_based = "./spec/mock/mappingbased_properties_en.nt"
+    @mapping_based = './spec/mock/mappingbased_properties_en.nt'
+    
     @clann = Clann.new(@mapping_based)
+
+    @invalid_triple_example = '<http://dbpedia.org/resource/Aristotle> <http://xmlns.com/foaf/0.1/name> ", Aristot\u00E9l\u0113s"@en .'
+    @valid_triple_example = '<http://dbpedia.org/resource/Animal_Farm> <http://dbpedia.org/ontology/author> <http://dbpedia.org/resource/George_Orwell> .'
+
   end
 
-  def test_should_read_input_file
+  def test_should_open_triple_set_file
     file = File.open(@mapping_based, 'r')
-    
     assert(File.identical?(@clann.triple_set, file))
   end
 
-  
+  def test_should_detect_invalid_uri
+    uri = '<http://dbpedia.org/resource/George_Orwell>'
+    assert(Clann.isDBpediaURI?(uri))
+
+    not_uri = 'Aristot\u00E9l\u0113s"@en'
+    assert_equal(Clann.isDBpediaURI?(not_uri), false)
+  end
+
+  def test_should_process_a_valid_triple
+    data = @clann.process_triple(@valid_triple_example)
+    assert_equal(data, ['<http://dbpedia.org/ontology/author>', ['<http://dbpedia.org/resource/Animal_Farm>','<http://dbpedia.org/resource/George_Orwell>']])
+  end
+
+  def test_should_ignored_invalid_triples
+    data = @clann.process_triple(@invalid_triple_example)
+    assert_equal(data, false)
+  end
+
+  def test_should_clusterize_predicates
+    clans = @clann.clusterize_triples
+    assert_equal(clans, {'<http://dbpedia.org/ontology/author>' => [['<http://dbpedia.org/resource/Animal_Farm>', '<http://dbpedia.org/resource/George_Orwell>']],
+                   '<http://dbpedia.org/ontology/philosophicalSchool>' => [['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Peripatetic_school>'], ['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Aristotelianism>']],
+                   '<http://dbpedia.org/ontology/mainInterest>' => [['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Physics>'], ['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Metaphysics>'], ['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Poetry>'], ['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Politics>']],
+                   '<http://dbpedia.org/ontology/notableIdea>' => [['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Reason>'], ['<http://dbpedia.org/resource/Aristotle>', '<http://dbpedia.org/resource/Logic>']]
+                 })
+  end
+
+  def test_should_count_triple_sets
+    number = @clann.count_triple_sets
+    assert_equal(number, 15)
+  end
+
+  def test_should_clear_out_inactive_memory
+    assert(@clann.free_the_fish)
+  end
+
+  def test_should_store_clusters
+  end
 end
--- src/clann.rb
+++ src/clann.rb
@@ -1,9 +1,92 @@
+require 'uri'
+require 'set'
+
 class Clann
   attr_reader :triple_set
 
   def initialize(filename)
     @triple_set = File.open(filename, 'r')
+    @triple_set_filename = filename
   end
 
-  
+  def self.isDBpediaURI?(uri)
+    unless uri[0] == '<' and uri[uri.size-1] == '>'
+      return false
+    end
+
+    uri = uri[1..uri.size-2]
+
+    begin
+      parse = URI(uri)
+      unless parse.scheme == "http" and parse.host == "dbpedia.org"
+        return false
+      end
+    rescue URI::InvalidURIError => e
+      return false
+    end
+
+    return true
+  end
+
+  def process_triple(triple)
+    spo = triple.split(" ")
+    s = spo[0].strip
+    p = spo[1].strip
+    o = spo[2].strip
+
+    unless Clann.isDBpediaURI?(o)
+      return false
+    else
+      return [p, [s,o]]
+    end
+  end
+
+  def count_triple_sets
+    line = 0
+
+    output = `wc -l #{@triple_set_filename}`
+
+    return output.split(" ").first.to_i-1
+  end
+
+  def free_the_fish
+    return `purge`
+  end
+
+  def clusterize_triples
+    clans = {}
+
+    print "Clearing out inactive RAM memory...\r"
+    free_the_fish
+
+    print "Counting triples on #{@triple_set_filename}\r"
+    number_triples = count_triple_sets
+    puts "Clustering #{number_triples} triples of #{@triple_set_filename}"
+
+    # Ignoring first comment line of DBpedia files
+    line = 0
+    triple_set.readline
+
+    @properties_index = Set.new    
+
+    triple_set.each_line do |t|
+      triple = process_triple(t)
+      line += 1
+
+      if triple
+        unless @properties_index.include? triple.first
+          clans[triple.first] = [triple.last]
+          @properties_index.add triple.first
+        else
+          clans[triple.first].push(triple.last)
+        end
+      end
+
+      if line % 10000 == 0
+        print "Processing line #{line}: #{(line*100)/number_triples}% complete\r"
+      end
+    end
+
+    return clans
+  end
 end
